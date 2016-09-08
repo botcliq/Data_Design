e1c16828d74ec49e9d36afa5d2d35b012838c3ba
Ashish Pundir <iamApalive@users.noreply.github.com>
2016-09-08 11:09:05 +0530

    Update forms.py

--- bin/mycircle/forms.py
+++ bin/mycircle/forms.py
@@ -1,6 +1,13 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField, TextAreaField
+from wtforms import StringField, BooleanField, TextField, TextAreaField, SubmitField
 from wtforms.validators import Required, Length
+
+class ContactForm(Form):
+  name = TextField("Name")
+  email = TextField("Email")
+  subject = TextField("Subject")
+  message = TextAreaField("Message")
+  submit = SubmitField("Send")
 
 class EditForm(Form):
     username = StringField('nickname', validators=[Required()])
