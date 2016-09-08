2a2f15e8cca4699b3cdae9c914218626beabfc62
Ashish Pundir <iamApalive@users.noreply.github.com>
2016-09-08 11:42:19 +0530

    Update views.py

--- bin/mycircle/views.py
+++ bin/mycircle/views.py
@@ -104,17 +104,20 @@ def edit():
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
     form = ContactForm()
-   
+ 
     if request.method == 'POST':
         if form.validate() == False:
-        flash('All fields are required.')
-        return render_template('contact.html', form=form)
-    else:
-        msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
-        msg.body = """
-        From: %s &lt;%s&gt;
-        %s
-        """ % (form.name.data, form.email.data, form.message.data)
-        mail.send(msg)
+            flash('All fields are required.')
+            return render_template('contact.html', form=form)
+        else:
+            msg = Message(form.subject.data, sender='contact@example.com', recipients=['your_email@example.com'])
+            msg.body = """
+            From: %s &lt;%s&gt;
+            %s
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
  
-    return render_template('contact.html', success=True)
+            return render_template('contact.html', success=True)
+ 
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
